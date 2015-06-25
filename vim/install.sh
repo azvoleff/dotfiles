adfab79d3eddee5fc9942e07a4a444651bcc0b6a
Alex Zvoleff <azvoleff@conservation.org>
2015-06-25 12:30:36 -0400

    Fix filenames.

---
+++ vim/install.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# httpos://github.com/captbaritone/dotfiles/blob/master/vim/install.sh
+
+echo "Updating/cleaning Vim plugins:"
+
+# Thanks for greywh in #vim for this tip
+vim -E -s <<-EOF
+    :source ~/.vimrc
+    :PlugInstall
+    :PlugClean
+    :qa
+EOF
