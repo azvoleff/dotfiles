deb7e19352bf13fd3afc49e45212cfe37f589259
Alex Zvoleff <azvoleff@conservation.org>
2015-12-21 07:54:57 -0500

    Install neovim from ppa.

--- neovim/install.sh
+++ neovim/install.sh
@@ -1,2 +1,14 @@
-git clone https://github.com/neovim/neovim.git ~/neovim
-make -C ~/neovim CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX:PATH=$HOME/neovim" install
+#!/bin/bash
+
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install neovim
+
+sudo apt-get install python-dev python-pip python3-dev python3-pip
+
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+sudo update-alternatives --config vi
+sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+sudo update-alternatives --config vim
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+sudo update-alternatives --config editor
